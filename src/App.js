470d64be24747e918dcf088c43f66a2fa7245bb6
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-22 23:45:16 -0600

    create pages and first with Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
 function App() {
-  return (
-  <Fragment>hello</Fragment>
-  );
+  return <Fragment>hello</Fragment>;
 }
 
 export default App;
