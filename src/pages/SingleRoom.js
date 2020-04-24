80f111d5e634e12a1fb80d1a4ee8dc8ce038fc56
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-24 07:49:01 -0600

    Single Room Hero

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,30 +1,54 @@
 import React, { Component } from "react";
-import defaultBcg from '../images/room-1.jpeg'
-import Hero from '../components/Hero'
-import Banner from '../components/Banner'
-import { Link } from 'react-router-dom'
-import {RoomContext} from '../context'
-
-
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
   constructor(props) {
-    super(props)
-    this.state ={
-      slug:this.props.match.params.slug,
-      defaultBcg
-    }
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
   }
 
-  static contextType = RoomContext
+  static contextType = RoomContext;
 
   // componentDidMount() {
 
   // }
   render() {
-    const {getRoom } = this.context
-    const room = getRoom(this.state.slug)
-  console.log(room)
-    return <div>Hello from single room page</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capactity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return <Hero hero='roomsHero' >
+      <Banner title={`${name} room`}>
+        <Link to='/rooms' className='btn-primary'>
+          back to rooms
+        </Link>
+      </Banner>
+    </Hero>
   }
 }
