381456dfbd32e27669d73fc9290a9a2eb07d5232
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-24 19:09:03 -0600

    render RoomContainer with other component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
+import RoomsConatiner from '../components/RoomsContainer'
 
 const Rooms = () => {
-  return <Hero hero="roomsHero">
-    <Banner title="our rooms">
-      <Link to="/" className="btn-primary">
-        return home
-      </Link>
-    </Banner>
-  </Hero>;
+  return (
+    <Fragment>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsConatiner />
+    </Fragment>
+  ); 
 };
 
 export default Rooms;
